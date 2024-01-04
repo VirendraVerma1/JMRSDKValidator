59790a9a07eeeade829616d847adf85918aa5457
Virendraverma1 <virendra@tesseract.in>
2024-01-04 17:55:59 +0530

    added examples scene, added manipulation rotation

---
+++ Assets/Validation/Example/Scripts/JMRDemoManipulationExample.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JMRSDK;
+
+public class JMRDemoManipulationExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        JMRRigManager.Instance.setHomePage = false;
+
+    }
+
+}
